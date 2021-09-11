b9421dba68949251ca4ddc08657bca84966a5b65
Abhijeet Singh <abhi178134@gmail.com>
2021-09-11 23:08:43 +0530

    Added Some Routes ..

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Components
 import Header from './components/Header';
 import Home from './components/Home';
+import News from './components/News';
+
+
 import { GlobalStyle } from './GlobalStyle';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
-  );
-}
+const App =() => (
+  <Router>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/:objectID' element={<News/>}/>
+      <Route path='/*' element={<Home/>}/>
+    </Routes>
+    <GlobalStyle/>
+  </Router>
+)
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Header />
+//       <Home />
+//       <GlobalStyle />
+//     </div>
+//   );
+// }
+
 export default App;
---
+++ src/components/News.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Spinner from "./Spinner";
+// import {useNewsFetch} from '../hook/useNewsFetch';
+
+const News = () => {
+    const { objectID } = useParams();
+    //const {state: news, loading, error } = useMovieFetch(movieId);
+    // if (loading) return <Spinner/>;
+    // if (error) return <div>Something Went Wrong...</div>;
+
+    return(
+        <>
+            Will Add News Here!!
+        </>
+    )
+
+}
+export default News;
