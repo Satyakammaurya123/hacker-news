ded74ae338865d8063952aab3e90c2729c1b8587
Abhijeet Singh <abhi178134@gmail.com>
2021-09-12 19:00:07 +0530

    Filtered JSON Data

--- src/components/News.js
+++ src/components/News.js
@@ -12,9 +12,12 @@ const News = () => {
     if (loading) return <Spinner/>;
     if (error) return <div>Something Went Wrong...</div>;
     //console.log(state);
-    const childrenText = news.children.map(childrens => (
-        <li>{childrens.text}</li>
-    ))
+    
+    const childrenText = news.children.flatMap(childrens => childrens.text 
+        ? <li>{childrens.text.replace(/<[^>]*>?/gm, '')}</li>
+        : []
+        // <li>{childrens.text && childrens.text.replace(/<[^>]*>?/gm, '')}</li>
+    );
     return(
         <>
         <h3>{news.title}</h3>
